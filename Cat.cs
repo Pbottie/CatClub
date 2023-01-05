b74a5b03a7e4888d613c7a77dc372771d3aad616
Kaami <pbottie@gmail.com>
2023-01-05 22:35:55 +0100

    Full working version, sans err check

---
+++ Cat.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatClub
+{
+    class Cat
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public int cat_id { get; set; }
+        public string name { get; set; }
+        public double weight { get; set; }
+        public int age { get; set; }
+        public string[] colors { get; set; }
+        public string gender { get; set; }
+
+        public Cat(int cat_id, string name, double weight, int age, string colors, string gender)
+        {
+            try
+            {
+
+            this.cat_id = cat_id;
+            this.name = name;
+            this.weight = weight;
+            this.age = age;
+            
+            string[] color = colors.Split(',');
+            this.colors = color;
+
+            this.gender = gender;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Input error! " + ex);
+                throw;
+            }
+        }
+
+        public override string ToString()
+        {
+            string allColors = "";
+
+            foreach (var color in colors)
+            {
+                allColors += color + " ";
+            }
+
+            return $"Cat ID: {cat_id} \nName: {name} \nWeight: {weight} \nAge: {age} \nColors: {allColors} \nGender: {gender}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 
 namespace CatClub
 {
@@ -7,120 +8,306 @@ namespace CatClub
     {
         static void Main(string[] args)
         {
-            MongoClient dbClient = new MongoClient("mongodb+srv://kaami:F6YS1WxP5wCm4X3c@test.hzlhdoo.mongodb.net/test");
-
+            var dbClient = new MongoClient("mongodb+srv://kaami:F6YS1WxP5wCm4X3c@test.hzlhdoo.mongodb.net/test");
             var database = dbClient.GetDatabase("CatClub");
             var collection = database.GetCollection<BsonDocument>("Cats");
 
-            #region Populate with Cats
-            List<BsonDocument> documents = new List<BsonDocument>{
-                new BsonDocument{
-                { "cat_id", 1 },
-                { "name", "Ginger"},
-                { "weight", 4.8},
-                {"age", 2 }
-                },  new BsonDocument{
-                { "cat_id", 2 },
-                { "name", "Max"},
-                { "weight", 3.6},
-                {"age", 3 }
-                },  new BsonDocument{
-                { "cat_id", 3 },
-                { "name", "Miku"},
-                { "weight", 5.75},
-                {"age", 10 }
-                },
-                new BsonDocument{
-                { "cat_id", 4 },
-                { "name", "Joker"},
-                { "weight", 3.8},
-                {"age", 5 }
-                },
-                new BsonDocument{
-                { "cat_id", 5 },
-                { "name", "Luna"},
-                { "weight", 5.8},
-                {"age", 15 }
-                },
-                new BsonDocument{
-                { "cat_id", 6 },
-                { "name", "Fren"},
-                { "weight", 4.82},
-                {"age", 7 }
-                }
-            };
 
-            //collection.InsertMany(documents);
-            #endregion
+            int mainMenuChoice = 0;
 
-            #region Read Cats
-            Console.WriteLine("First Cat");
-            var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
-
-            Console.WriteLine(firstDocument.ToString());
-
-
-            Console.WriteLine("Cat with id of 2");
-            var filter = Builders<BsonDocument>.Filter.Eq("cat_id", 2);
-            var catDocument = collection.Find(filter).FirstOrDefault();
-            Console.WriteLine(catDocument.ToString());
-
-
-            Console.WriteLine("All cats");
-            var allCats = collection.Find(new BsonDocument()).ToList();
-            foreach (var cat in allCats)
+            do
             {
-                Console.WriteLine(cat.ToString());
+                mainMenuChoice = MainMenu();
+
+
+                switch (mainMenuChoice)
+                {
+                    case 1:
+                        SelectCat(collection);
+                        break;
+                    case 2:
+                        AddCat(collection);
+                        break;
+                    case 3:
+                        ListCats(collection);
+                        break;
+                    case 4:
+                        RemoveCat(collection);
+                        break;
+                    case 5:
+                        UpdateCat(collection);
+                        break;
+                    case 6:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+
+            }
+            while (mainMenuChoice != 6);
+
+
+
+        }
+
+        static int MainMenu()
+        {
+            Console.WriteLine("~~~~~~~~~~CatClub~~~~~~~~~~");
+            Console.WriteLine("1. Select Cat");
+            Console.WriteLine("2. Add Cat");
+            Console.WriteLine("3. Show all Cats");
+            Console.WriteLine("4. Remove Cat");
+            Console.WriteLine("5. Update Cat");
+            Console.WriteLine("6. Exit");
+
+            int mainMenuOptions = 6;
+            int input = GetValidInput(mainMenuOptions);
+
+            return input;
+
+
+        }
+
+        static void SelectCat(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose Cat ID");
+
+            int catID;
+            FilterDefinition<BsonDocument> filter;
+
+            if (Int32.TryParse(Console.ReadLine(), out catID))
+            {
+                filter = Builders<BsonDocument>.Filter.Eq("cat_id", catID);
+                var catDocument = collection.Find(filter).FirstOrDefault();
+
+                if (catDocument == null)
+                    Console.WriteLine("No cat with that Id was found.");
+                else
+                {
+
+                    var cat = BsonSerializer.Deserialize<Cat>(catDocument);
+                    Console.WriteLine(cat);
+                }
+
+            }
+            else
+                Console.WriteLine("Invalid integer");
+
+
+
+
+        }
+
+        static void AddCat(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine("Input Cat Data: ");
+            Cat cat;
+
+            try
+            {
+                Console.Write("Id(int): ");
+                int id = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Name(string): ");
+                string name = Console.ReadLine();
+
+                Console.Write("Weight(double): ");
+                double weight = Double.Parse(Console.ReadLine());
+
+                Console.Write("Age(int): ");
+                int age = Int32.Parse(Console.ReadLine());
+
+                Console.Write("colors(string) separate with ,: ");
+                string colors = Console.ReadLine();
+
+                Console.Write("Gender(string): ");
+                string gender = Console.ReadLine();
+
+                cat = new Cat(id, name, weight, age, colors, gender);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                throw;
             }
 
-            Console.WriteLine("Fat cat");
-
-            var fattestCatFilter = Builders<BsonDocument>.Filter.Eq(
-                 "weight", new BsonDocument { { "$gte", 4 } });
-
-            var sort = Builders<BsonDocument>.Sort.Descending("weight");
-
-            var fattestCat = collection.Find(fattestCatFilter).Sort(sort).First();
-
-            Console.WriteLine(fattestCat);
-            #endregion
-
-            #region Update Cats
-            Console.WriteLine("Update cat with id of 2");
-            var updateFilter = Builders<BsonDocument>.Filter.Eq("cat_id", 2);
-            var update = Builders<BsonDocument>.Update.Set("name", "Maxilluim");
-            collection.UpdateOne(updateFilter, update);
-            catDocument = collection.Find(filter).FirstOrDefault();
-            Console.WriteLine(catDocument.ToString());
+            if (cat != null)
+                collection.InsertOne(cat.ToBsonDocument());
 
 
-            #endregion
+
+        }
+
+        static void ListCats(IMongoCollection<BsonDocument> collection)
+        {
+            var allCats = collection.Find(new BsonDocument()).ToList();
+
+            foreach (var catson in allCats)
+            {
+                var cat = BsonSerializer.Deserialize<Cat>(catson);
+                Console.WriteLine(cat);
+                Console.WriteLine();
+            }
+        }
+
+        static void RemoveCat(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose Cat ID to delete");
+
+            int catID;
+            FilterDefinition<BsonDocument> deleteFilter;
+
+            if (Int32.TryParse(Console.ReadLine(), out catID))
+            {
+                deleteFilter = Builders<BsonDocument>.Filter.Eq("cat_id", catID);
+                var catDocument = collection.Find(deleteFilter).FirstOrDefault();
+
+                if (catDocument == null)
+                    Console.WriteLine("No cat with that Id was found.");
+                else
+                {
+                    var cat = BsonSerializer.Deserialize<Cat>(catDocument);
+                    collection.DeleteOne(deleteFilter);
+                    Console.WriteLine(cat + "\nWas deleted");
+                }
+
+            }
+            else
+                Console.WriteLine("Invalid integer");
+
+        }
+
+        static void UpdateCat(IMongoCollection<BsonDocument> collection)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose Cat ID");
+
+            int catID;
+            FilterDefinition<BsonDocument> updateFilter;
+
+            if (Int32.TryParse(Console.ReadLine(), out catID))
+            {
+                updateFilter = Builders<BsonDocument>.Filter.Eq("cat_id", catID);
+                var catDocument = collection.Find(updateFilter).FirstOrDefault();
+                var fields = catDocument.Count();
+
+                if (catDocument == null)
+                    Console.WriteLine("No cat with that Id was found.");
+                else
+                {
+                    Console.WriteLine("What would you like to update?");
 
 
-            #region Delete Cats
+                    var enumTest = catDocument.Names;
+                    for (int i = 1; i < fields; i++)
+                    {
+                        Console.WriteLine($"{i}. {catDocument.GetElement(i)}");
+                    }
+
+                    int input = GetValidInput(fields - 1);
+                    var updateName = catDocument.GetElement(input).Name;
+
+                    Console.Write($"Update {updateName}: ");
+                    if (catDocument.GetElement(input).Value.IsBsonArray)
+                    {
+                        Console.Write("(use , to seperate colors) ");
+                        BsonArray updateArray = new BsonArray();
+                        string[] strArr = Console.ReadLine().Split(',');
+                        foreach (var color in strArr)
+                        {
+                            updateArray.Add(color);
+                        }
+                        var update = Builders<BsonDocument>.Update.Set(updateName, updateArray);
+                        collection.UpdateOne(updateFilter, update);
+
+                    }
+                    else
+                    {
+                        string updateValue = Console.ReadLine();
+                        var update = Builders<BsonDocument>.Update.Set(updateName, updateValue);
+                        collection.UpdateOne(updateFilter, update);
+
+                    }
+
+                }
+
+            }
+            else
+                Console.WriteLine("Invalid integer");
 
 
-            #endregion
+
+            //Console.WriteLine("Update cat with id of 2");
+            //var updateFilter = Builders<BsonDocument>.Filter.Eq("cat_id", 2);
+
 
         }
 
 
 
+        static int GetValidInput(int menuOptions)
+        {
+            bool inmatat = false;
+            int output = 0;
+
+            while (!inmatat)
+            {
+                try
+                {
+                    Console.Write("Make selection: ");
+                    inmatat = int.TryParse(Console.ReadLine(), out output);
+                    if (!inmatat)
+                        Console.WriteLine("Integers only please!");
+                    else if (output > menuOptions || output < 1)
+                    {
+                        Console.WriteLine("Invalid selection");
+                        inmatat = false;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Readline failed with message: " + ex.Message);
+                }
+
+            }
+            return output;
+        }
+
+        static bool GetValidBool()
+        {
+            char answer = 'c';
+
+            while (answer != 'y' && answer != 'n')
+            {
+                Console.WriteLine("Are you sure? (y/n): ");
+                try
+                {
+
+                    answer = (char)Console.Read();
+                    Console.ReadLine();//Gets rid of pesky "". Don't want to do Console.ReadLine().Trim()[0]
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Read failed with message: " + ex.Message);
+                }
+            }
+
+            if (answer == 'y')
+                return true;
+            return false;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
 
